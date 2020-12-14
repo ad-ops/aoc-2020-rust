5fe6978e73086f086799b3501b1bfcabb20558f9
ad-ops <adrian.opsahl@gmail.com>
2020-12-14 09:50:20 +0100

    added preprocess step in macros to be able to change raw String input before splitting into Vec<String>.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
 pub struct Puzzle<'a> {
     day: &'a str,
+    pre_processor: fn(String) -> String,
 }
 impl<'a> Puzzle<'a> {
-    pub fn new(day: &'a str) -> Self {
+    pub fn new(day: &'a str, pre_processor: fn(String) -> String) -> Self {
         Puzzle {
-            day
+            day,
+            pre_processor,
         }
     }
 
     pub fn solve(&self, solver: fn(Vec<String>) -> String) -> String {
-        let input = get_input(DayInput::Input(self.day));
+        let input = get_input(DayInput::Input(self.day), self.pre_processor);
         solver(input)
     }
 
     pub fn test(&self, solver: fn(Vec<String>) -> String) -> String {
-        let input = get_input(DayInput::Test(self.day));
+        let input = get_input(DayInput::Test(self.day), self.pre_processor);
         solver(input)
     }
 }
@@ -24,13 +26,14 @@ pub enum DayInput<'a> {
     Test(&'a str),
 }
 
-pub fn get_input(day: DayInput) -> Vec<String> {
+pub fn get_input(day: DayInput, pre_processor: fn(String) -> String) -> Vec<String> {
     let path = match day {
         DayInput::Input(d) => format!("input/{}_input.txt", d),
         DayInput::Test(d) => format!("input/{}_test.txt", d),
     };
-    let lines = std::fs::read_to_string(path)
-        .expect("Could not find file!")
+    let input = std::fs::read_to_string(path)
+        .expect("Could not find file!");
+    let lines = pre_processor(input)
         .lines()
         .map(|l| l.to_string() )
         .collect::<Vec<String>>();
@@ -39,11 +42,28 @@ pub fn get_input(day: DayInput) -> Vec<String> {
 
 #[macro_export]
 macro_rules! puzzle_main {
+    ($solver_part1:expr, $solver_part2:expr, $preprocess:tt) => {
+        fn main() {
+            let day = env!("CARGO_BIN_NAME");
+            println!("Advent of Code 2020 - {}", day);
+            let puzzle = Puzzle::new(day, $preprocess);
+            let part1_start = std::time::Instant::now();
+            let solution_part1 = puzzle.solve($solver_part1);
+            let part1_time = part1_start.elapsed().as_micros();
+            let part2_start = std::time::Instant::now();
+            let solution_part2 = puzzle.solve($solver_part2);
+            let part2_time = part2_start.elapsed().as_micros();
+            println!("{} - Part 1 solution took {}µs", day, part1_time);
+            println!("{}", solution_part1);
+            println!("{} - Part 2 solution took {}µs", day, part2_time);
+            println!("{}", solution_part2);
+        }
+    };
     ($solver_part1:expr, $solver_part2:expr) => {
         fn main() {
             let day = env!("CARGO_BIN_NAME");
             println!("Advent of Code 2020 - {}", day);
-            let puzzle = Puzzle::new(day);
+            let puzzle = Puzzle::new(day, |x| x);
             let part1_start = std::time::Instant::now();
             let solution_part1 = puzzle.solve($solver_part1);
             let part1_time = part1_start.elapsed().as_micros();
@@ -60,6 +80,26 @@ macro_rules! puzzle_main {
 
 #[macro_export]
 macro_rules! puzzle_tests {
+    ($expected_part1:expr, $expected_part2:expr, $preprocess:tt) => {
+        #[cfg(test)]
+        mod aoc_test {
+            use super::*;
+
+            #[test]
+            fn puzzle_part1_test() {
+                let day = env!("CARGO_BIN_NAME");
+                let puzzle = Puzzle::new(day, $preprocess);
+                assert_eq!(puzzle.test(solver_part1), $expected_part1);
+            }
+
+            #[test]
+            fn puzzle_part2_test() {
+                let day = env!("CARGO_BIN_NAME");
+                let puzzle = Puzzle::new(day, $preprocess);
+                assert_eq!(puzzle.test(solver_part2), $expected_part2);
+            }
+        }
+    };
     ($expected_part1:expr, $expected_part2:expr) => {
         #[cfg(test)]
         mod aoc_test {
@@ -68,14 +108,14 @@ macro_rules! puzzle_tests {
             #[test]
             fn puzzle_part1_test() {
                 let day = env!("CARGO_BIN_NAME");
-                let puzzle = Puzzle::new(day);
-                assert_eq!(puzzle.test(solver_part1), $expected_part1);
+                let puzzle = Puzzle::new(day, |x| x);
+                assert_eq!(puzzle.test(solver_part1), $expected_part1,);
             }
 
             #[test]
             fn puzzle_part2_test() {
                 let day = env!("CARGO_BIN_NAME");
-                let puzzle = Puzzle::new(day);
+                let puzzle = Puzzle::new(day, |x| x);
                 assert_eq!(puzzle.test(solver_part2), $expected_part2);
             }
         }
@@ -97,13 +137,13 @@ mod test {
 
     #[test]
     fn puzzle_solve() {
-        let puzzle = Puzzle::new("day0");
+        let puzzle = Puzzle::new("day0", |x| x);
         assert_eq!("15".to_string(), puzzle.solve(solver));
     }
 
     #[test]
     fn puzzle_test() {
-        let puzzle = Puzzle::new("day0");
+        let puzzle = Puzzle::new("day0", |x| x);
         assert_eq!("30".to_string(), puzzle.test(solver));
     }
 }
